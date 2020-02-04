627308e12a378ba9371550d2b264962eb3fec4cd
hisona <hisona.developer@gmail.com>
2020-02-04 14:08:10 +0900

    intent uri decode 추가

--- app/src/main/java/com/mxtech/videoplayer/pro/MainActivity.java
+++ app/src/main/java/com/mxtech/videoplayer/pro/MainActivity.java
@@ -16,6 +16,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     private final static String TAG = MainActivity.class.getName();
@@ -69,11 +71,68 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private static int hexToBin( char ch ) {
+        if( '0'<=ch && ch<='9' )    return ch-'0';
+        if( 'A'<=ch && ch<='F' )    return ch-'A'+10;
+        if( 'a'<=ch && ch<='f' )    return ch-'a'+10;
+        return -1;
+    }
+
+    private static byte[] parseHexBinary(String s) {
+        final int len = s.length();
+
+        // "111" is not a valid hex encoding.
+        if( len%2 != 0 )
+            throw new IllegalArgumentException("hexBinary needs to be even-length: "+s);
+
+        byte[] out = new byte[len/2];
+
+        for( int i=0; i<len; i+=2 ) {
+            int h = hexToBin(s.charAt(i  ));
+            int l = hexToBin(s.charAt(i+1));
+            if( h==-1 || l==-1 )
+                throw new IllegalArgumentException("contains illegal character for hexBinary: "+s);
+
+            out[i/2] = (byte)(h*16+l);
+        }
+
+        return out;
+    }
+
+    private static final char[] hexCode = "0123456789abcdef".toCharArray();
+
+    private static String printHexBinary(byte[] data) {
+        StringBuilder r = new StringBuilder(data.length*2);
+        for ( byte b : data) {
+            r.append(hexCode[(b >> 4) & 0xF]);
+            r.append(hexCode[(b & 0xF)]);
+        }
+        return r.toString();
+    }
+
     public static String intentToString(Intent intent) {
         if (intent == null)
             return "";
 
         String str = Uri.decode(intent.getDataString());
+
+        String data = intent.getDataString();
+
+        if(data != null) {
+            int pos = data.indexOf("dlink=");
+            if(pos > 0) {
+
+                String dlink = data.substring(pos+9, data.indexOf("%22", pos+9));
+                String decode = new String(parseHexBinary(dlink));
+                String encode = printHexBinary(decode.getBytes());
+
+                Log.e(TAG, dlink);
+                Log.e(TAG, decode);
+                Log.e(TAG, encode);
+                Log.e(TAG, "compare: " + Boolean.toString(dlink.equals(encode)));
+
+            }
+        }
 
         StringBuilder stringBuilder = new StringBuilder("action: ")
                 .append(intent.getAction())
